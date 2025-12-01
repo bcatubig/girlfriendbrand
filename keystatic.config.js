ba2298f121994000056e356f957670dc14171b19
Brandon Catubig <bcatubig@pm.me>
2025-11-30 16:47:38 -0800

    updates

--- keystatic.config.js
+++ keystatic.config.js
@@ -6,15 +6,58 @@ export default config({
     kind: "local",
   },
   collections: {
-    posts: collection({
-      label: "Posts",
+    products: collection({
+      label: "Products",
       slugField: "title",
-      path: "src/content/posts/*",
+      path: "src/content/products/*/",
 
       format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: "Title" } }),
-        content: fields.markdoc({ label: "Content" }),
+        title: fields.slug({
+          name: { label: "Product Title" },
+        }),
+        cover: fields.image({
+          label: "Cover Image",
+          description: "The primary product image displayed on the homepage",
+          directory: "src/assets/images/products",
+          publicPath: "@assets/images/products/",
+        }),
+        category: fields.select({
+          label: "Product Category",
+          defaultValue: "designer",
+          options: [
+            { label: "Designer", value: "designer" },
+            { label: "Developer", value: "developer" },
+            { label: "Product manager", value: "product-manager" },
+          ],
+        }),
+        price_usd: fields.number({
+          label: "Price $",
+          step: 1,
+          defaultValue: 0,
+          validation: {
+            min: 0,
+          },
+        }),
+        productImages: fields.array(
+          fields.image({
+            label: "Product Image",
+            description: "Add a new product image",
+            validation: { isRequired: true },
+            directory: "src/assets/images/products",
+            publicPath: "@assets/images/products/",
+          }),
+          {
+            label: "Product Images",
+            description: "Additional product images",
+            itemLabel: (props) => props.value.filename,
+          },
+        ),
+        specifications: fields.array(fields.text({}), {
+          label: "Specifications",
+          itemLabel: (props) => props.value,
+        }),
+        content: fields.markdoc({ label: "Product Description" }),
       },
     }),
   },
